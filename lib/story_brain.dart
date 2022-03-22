3a1842767644ad04ad5dbec8f4a5be37607148d1
Lani <lani.mortontran@movemedical.com>
2022-03-22 15:38:40 -0700

    Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,7 +1,7 @@
 import 'package:destini_challenge_starting/story.dart';
 
 class StoryBrain {
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   List<Story> _storyData = [
     Story(
@@ -27,32 +27,28 @@ class StoryBrain {
         choice2: '')
   ];
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
   String getStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData[0].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData[0].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
   void nextStory(int choiceNumber) {
-    switch (storyNumber) {
+    switch (_storyNumber) {
       case 0:
-        choiceNumber == 1 ? storyNumber = 2 : storyNumber = 1;
+        choiceNumber == 1 ? _storyNumber = 2 : _storyNumber = 1;
         break;
       case 1:
-        choiceNumber == 1 ? storyNumber = 2 : storyNumber = 3;
+        choiceNumber == 1 ? _storyNumber = 2 : _storyNumber = 3;
         break;
       case 2:
-        choiceNumber == 1 ? storyNumber = 5 : storyNumber = 4;
+        choiceNumber == 1 ? _storyNumber = 5 : _storyNumber = 4;
         break;
       default:
         restart();
@@ -60,7 +56,7 @@ class StoryBrain {
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
